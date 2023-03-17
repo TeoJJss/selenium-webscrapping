60f5757fdc0850ea413cbe565209b70b9f8ae47c
TeoJJss <teojunjia@gmail.com>
2023-03-17 20:51:22 +0800

    initial

---
+++ README.md
@@ -0,0 +1,20 @@
+This project is to extract results from SPM official website.  
+Visit https://gvs.moe.gov.my/qr/ for details.  
+
+<b>DO THIS FIRST</b>
+Input to the project should be an image of SPM certificate with a visible QR code.  
+Add the image path and student's examination number (angka giliran) in main.py file.  
+Supported file format of image: jpg, png, jpeg  
+
+<b>PROCESS</b>
+The algorithm will look for QR code in the SPM certificate file.  
+And then, the algorithm will proceed to the official website's URL that is obtained from the image file.  
+Lastly, the algorithm will return the texts obtained from the website.  
+We can add algorithm to classify the texts in the future.  
+
+<b>COMMANDS</b>
+To run, please ensure to use Python 3.9 or above, and run the following commands:  
+```
+pip install -r requirements.txt  
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import io
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+# CHANGE THIS
+path="<image_path>" #put your image file path here
+ag="<angka_giliran>" #put the student's examniation number
+
+img = Image.open(path)
+qr = decode(img)
+url=""
+for d in qr:
+    if d.type == "QRCODE":
+        url = d.data.decode()
+        break
+
+print("url: https://"+url)
+
+options = Options()
+options.add_argument('--headless')
+# options.add_argument('--no-sandbox')
+# options.add_argument('--single-process')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(chrome_options=options)
+
+driver.get("https://"+url)
+driver.maximize_window() # For maximizing window
+driver.implicitly_wait(20) # gives an implicit wait for 20 seconds
+
+wait = WebDriverWait(driver, 10)
+inp=driver.find_element("name", "ag") #input examination number
+
+inp.click()
+inp.send_keys(ag)
+submit = driver.find_element(By.XPATH, '//button[text()="Semak"]')
+submit.click()
+elem = driver.find_element("id", "rec")
+# Get data after submit at the website
+response = {
+    "statusCode": 200,
+    "body": elem.text
+}
+print(response)
+driver.close()
+driver.quit()
+
+# return response
